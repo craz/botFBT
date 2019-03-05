e35c34a4f3ad71763659c245d53d6fd87dc34569
Алексей Павлов <pavlov.av@lime-shop.ru>
2019-03-05 10:05:26 +0400

    hide real config

---
+++ config_example.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright (c) 2019 Alex Pavlov
+ * Created: 03.03.2019
+ * Author: Alex Pavlov
+ * Email: cccrazzz@gmail.com
+ */
+return [
+    'facebook' => [
+        'app_id' => "",
+        'app_secret' => "",
+        'cookie' => true,
+        'app_access_token' => "",
+        'page_marker' => "
+    ],
+    'telegram' => [
+        'access_token' => '',
+        'bot_name' => ''
+    ],
+    'mysqli' => [
+        'host'=>'localhost',
+        'user'=>'root',
+        'password'=>'',
+        'db'=>'botFBT'
+    ]
+];
